18ec53d5d5131664f2d54e8c4a3e3ddb25af70e2
ficolas2 <nicolasdepg@gmail.com>
2025-09-15 22:48:00 +0100

    feat: open table

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::sync::Arc;
-
 use deltalake::datafusion::prelude::SessionContext;
-use deltalake::open_table;
+use open_table::open_table_command;
 use rustyline::error::ReadlineError;
 use rustyline::{DefaultEditor, Result};
 
+pub mod open_table;
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    deltalake::aws::register_handlers(None);
     let ctx = SessionContext::new();
     
     let mut rl = DefaultEditor::new()?;
@@ -14,6 +15,10 @@ async fn main() -> Result<()> {
         let readline = rl.readline("deltaq> ");
         match readline {
             Ok(line) if matches!(line.trim(), "quit" | "exit" | "\\q") => break,
+            Ok(line) if line.starts_with(".") => {
+                rl.add_history_entry(line.as_str())?;
+                run_command(&ctx, &line).await;
+            }
             Err(ReadlineError::Interrupted) => {
                 println!("^C");
             }
@@ -30,3 +35,14 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+async fn run_command(ctx: &SessionContext, line: &str) {
+    let args = shell_words::split(line).expect("parse failed");
+
+    match args[0].as_str() {
+        ".open" => {
+            open_table_command(ctx, line).await;
+        }
+        _ => {}
+    }
+
+}
---
+++ src/open_table.rs
@@ -0,0 +1,73 @@
+use std::{collections::HashMap, sync::Arc};
+
+use clap::{Parser, arg, command};
+use deltalake::{datafusion::prelude::SessionContext, open_table_with_storage_options};
+
+#[derive(Parser, Debug)]
+#[command(name = "open", about = "Open a Delta table")]
+struct OpenArgs {
+    /// Logical name you want to assign
+    table_name: String,
+
+    /// s3://bucket/path
+    table_path: String,
+
+    /// http://host:port for MinIO / S3 endpoint
+    #[arg(long)]
+    endpoint_url: Option<String>,
+
+    /// Access key
+    #[arg(long)]
+    access_key_id: Option<String>,
+
+    /// Secret key
+    #[arg(long)]
+    secret_access_key: Option<String>,
+
+    /// true/false
+    #[arg(long, default_value_t = true, action = clap::ArgAction::Set)]
+    allow_http: bool,
+
+    /// S3 addressing style: "path" or "virtual"
+    #[arg(long, value_parser = ["path", "virtual"], default_value = "path")]
+    addressing_style: String,
+
+    /// Conditional write mechanism: "etag" (MinIO) or "dynamodb" (AWS)
+    #[arg(long, value_parser = ["etag", "dynamodb"], default_value = "etag")]
+    conditional_put: String,
+}
+
+pub async fn open_table_command(ctx: &SessionContext, line: &str) {
+    let args = match OpenArgs::try_parse_from(shell_words::split(line).expect("parse failed")) {
+        Ok(args) => args,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+
+    let mut storage_options: HashMap<String, String> = HashMap::new();
+
+    if let Some(v) = args.endpoint_url {
+        storage_options.insert("AWS_ENDPOINT_URL".into(), v);
+    }
+    if let Some(v) = args.access_key_id {
+        storage_options.insert("AWS_ACCESS_KEY_ID".into(), v);
+    }
+    if let Some(v) = args.secret_access_key {
+        storage_options.insert("AWS_SECRET_ACCESS_KEY".into(), v);
+    }
+
+    storage_options.insert("AWS_ALLOW_HTTP".into(), args.allow_http.to_string());
+    storage_options.insert("AWS_S3_ADDRESSING_STYLE".into(), args.addressing_style);
+    storage_options.insert("aws_conditional_put".into(), args.conditional_put);
+
+    let table = match open_table_with_storage_options(args.table_path, storage_options).await {
+        Ok(table) => Arc::new(table),
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+    ctx.register_table(args.table_name, table).unwrap();
+}
